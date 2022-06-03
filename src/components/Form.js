306920c8897c51be082c68a0c4115225ed0a563a
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-06-02 18:54:06 -0700

    Completed assignment

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+function Form({firstName, lastName, newsletter, handleFirstNameChange, handleLastNameChange, handleNewsletterChange}) {
 
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} /><br></br>
+      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+      <input
+        type="checkbox"
+        id="newsletter"
+        onChange={handleNewsletterChange}
+        checked={newsletter}
+      /><br></br>
       <button type="submit">Submit</button>
     </form>
   );
